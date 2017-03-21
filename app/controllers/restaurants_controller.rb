33733004e8fbd9a5d505373fe36d1660e4e06b83
Ben <ben@ben.kleinschmidt.com>
2017-03-21 10:40:29 -0500

    added authentication to yelp. api is working

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class RestaurantsController < ApplicationController
+
+	def index
+
+		# binding.pry
+
+		# params = { term: 'food',
+  	#          limit: 3,
+  	#          category_filter: 'discgolf'
+  	#        }
+
+		# locale = { lang: 'fr' }
+		# client.search('San Francisco', params, locale)
+
+		Yelp.client.search('San Francisco',{term: 'food',limit: 3},{lang: 'en'})
+
+
+	end
+
+
+end
